04690649f037afa1d25404115a30515e9fd9f77e
Jliciaga <josiah.liciagasilva@gmail.com>
2018-11-30 13:05:19 -0500

    grammar changes top about page

--- client/src/pages/AboutPage/AboutPage.jsx
+++ client/src/pages/AboutPage/AboutPage.jsx
@@ -11,32 +11,38 @@ const AboutPage = (props) => {
         <div className="about-wrapper">
           <div className="about-content">
             <p>
-              Hello! My name is Josiah Liciaga Silva, 
-              fullstack software engineer.<strong> Welcome! </strong> 
-               to my portfolio.
+              Hello! My name is Josiah Liciaga Silva - FullStack Software Engineer. <strong>Welcome </strong>to my portfolio!
             </p>
             <p>
-              I have worked as a software engineer since early 2016, 
-              currently working as a freelancer for Elevate Entertainment 
-              either as a UX/UI designer or building fullstack websites and/or applications 
-              from scratch. I lived my childhood through my early teens in Cabo Rojo, Puerto Rico
-              before moving to the state of Florida in the United States. I have traveled far and wide
-              to different states in the US and to different coutries. My love for exploration has led me to discover
-              many beautiful locations across many different areas and has led me to discover new and exciting technologies.
+              I have been working as a software engineer since late 2015, am currently looking for a full-time position with a great company!
             </p>
             <p>
-              I have always loved the art or creative aspect that coding requires, the process of creating something new from scratch or
-              solving new or old problems, have led me to create many awesome applications with real world use. I built this site from absolute scratch. 
-              I didnt use any UI library or framework except for React as it is incredible useful to work with. I try to keep up to date with 
-              the pace of technology as it evolves, which then leads me to blog what ever I learn, because shared knowledge is the best knowledge.
+              I am fully bilingual (English/Spanish) as I lived my childhood through my early teens in Cabo Rojo, Puerto Rico before moving to the state of Florida.
             </p>
             <p>
-              I specialize and do most of my work using JavaScript and its libraries/frameworks, 
-              mainly using a MERN (MongoDB, ExpressJS, ReactJs and NodeJs) 
-              stack as it is my preferred stack and it allows me to create applications quickly, 
-              but have also done a good amount of work using AngularJS, Redux and WordPress.
-              I have a good amount of fundamental knowledge of the languages Python and C# and some their libraries/frameworks. 
-              I am also obsessed with blockchain and machine learning as I believe they are future.
+              Before switching to the Software Engineering field, I worked for many years as the lead Radiologic Technologist at Florida Hospital. Here I led a team of over 20 successful technologists.
+            </p>
+            <p>
+              For my Software Engineering education I attended Hack Reactor, located in San Francisco, CA. Hack Reactor is rated the #2* Software Engineering program in the country and has been referred to as the “Harvard of coding bootcamps.”** Here I had experience working and training with the best software engineers in the country and was able to learn invaluable education in the field.
+            </p>
+            <p>
+              I am passionate about software engineering and the creative aspect that coding requires. The process of creating a new technology from scratch or solving complicated problems has led me to create many advanced applications with real-world use. This website was built from scratch, and I always keep up-to-date with technology as it evolves.
+            </p>
+            <p>
+              I specialize and do most of my work using JavaScript and its libraries/frameworks. Mainly using a MERN (MongoDB, ExpressJS, ReactJs and NodeJs) as my preferred stack, and it allows me to create applications quickly.
+            </p>
+            <p>
+              I also have a lot of experience using AngularJS, Redux and WordPress, and the languages Python and C#. My favorites currently are blockchain and machine learning - I believe they are the future of software engineering.
+            </p>
+            <p>
+              I am looking for a great company with a diverse and distributed team where new and innovative ideas are appreciated. I enjoy being challenged and understand that continuing to learn new languages and development techniques are important to me and the success of your organization. I also consider myself a fun and collaborative teammate to work with! My hobbies outside of work include paintballing competitively, jiu-jitsu, and spinning fire knife. (Yes, real knives with fire on each end!)
+            </p>
+            <p>
+              Please feel free to explore my portfolio website and check out my projects and resume. Thank you for your time and consideration. I look forward to speaking with you more about your needs and my abilities.
+            </p>
+            <p>
+              Best Regards,<br/>
+                Josiah Liciaga-Silva
             </p>
           </div>
         </div>
