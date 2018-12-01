61064bfb0e528e0d82e464abf8ae8694e8e70b2c
Jliciaga <josiah.liciagasilva@gmail.com>
2018-11-30 22:42:10 -0500

    hotfix

--- client/src/App.js
+++ client/src/App.js
@@ -18,9 +18,9 @@ export default class App extends Component {
       const ele = document.getElementById('ipl-progress-indicator')
       if(ele) {
         ele.classList.add('available')
-        // setTimeout(() => {
-        //   ele.outerHTML = ''
-        // }, 2000)
+        setTimeout(() => {
+          ele.outerHTML = ''
+        }, 2000)
       }
     });
   };
